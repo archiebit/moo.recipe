058ab179200e3a291542f896b48337892d3a0c39
Gregory Kramar <archiebit8@gmail.com>
2026-03-12 18:14:58 +0300

    Implement output of error messages

--- source/cook.exe/error.cc
+++ source/cook.exe/error.cc
@@ -9,6 +9,45 @@
 
 namespace moo
 {
+    // Fancy text formatting.
+    enum format
+    {
+        RESET,
+
+        BOLD,
+        UNDERLINE,
+
+        RED,
+        WHITE,
+        YELLOW,
+        GRAY
+    };
+
+
+    std::ostream & operator << ( std::ostream & stream, format option )
+    {
+        // Using ANSI escape sequences.
+        switch( option )
+        {
+            case RESET:     stream << "\x1B[0m";  break;
+
+            case BOLD:      stream << "\x1B[1m";  break;
+            case UNDERLINE: stream << "\x1B[4m";  break;
+
+            case RED:       stream << "\x1B[31m"; break;
+            case WHITE:     stream << "\x1B[37m"; break;
+            case YELLOW:    stream << "\x1B[93m"; break;
+            case GRAY:      stream << "\x1B[90m"; break;
+        }
+
+
+        return stream;
+    }
+}
+
+
+namespace moo
+{
     error::~error( ) = default;
 
 
@@ -36,7 +75,11 @@ namespace moo
 
     void error::print_message( ) const
     {
-        // TODO: Implement error message output.
+        std::cerr << RESET;
+
+        std::cerr << RED << "Error: " << UNDERLINE << WHITE << message;
+
+        std::cerr << RESET << '\n';
     }
 
 
